9b2b6ebed916921f2e4674ebcf63a1aa1d31e8db
Haruka1551 <haruka.yamaguchi02@gmail.com>
2025-07-17 13:22:21 +0900

    Add files via upload

---
+++ kadai_020/event.js
@@ -0,0 +1,9 @@
+const addText =document.getElementById('text');
+const pushBtn =document.getElementById('btn');
+
+pushBtn.addEventListener('click',()=> {
+  
+  addText.textContent = 'ボタンをクリックしました';
+ 
+})
+
